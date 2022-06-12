8c102be59bd3e085b85eb946922daefa8e316c1e
Hoffmann-Ferreira <andreihf@outlook.com>
2022-06-12 01:19:54 -0300

    Feat: Modal de adcionar paletas e sua integração

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,16 +1,31 @@
 import "./style.css";
 import logo from "../../assets/icons/logo.svg";
 import sacola from "../../assets/icons/sacola.png";
+import {toast} from "react-hot-toast";
+import ModalNewIceCream from "../Modals/NewIceCream";
+import { useState } from "react";
 
 const Header = () => {
+  const [displayModalCreate, setDisplayModalCreate] = useState(false);
+
+  const handleDisplayModalCreate = () => {
+    setDisplayModalCreate(!displayModalCreate)
+  }
+
+
+
   return (
+  <>
     <header>
       <div className="cabecalho">
         <img className="img-geladon" src={logo} alt="Logo El Geladon" />
         <h2> El Geladon </h2>
-        <img src={sacola} alt="Logo de uma socola" />
+        <img className="sacola" src={sacola} alt="Logo de uma socola" onClick={() => toast.error("sessão em desenvolvimento")} />
+        <button onClick={handleDisplayModalCreate} className="botao-Adicionar">Adcionar paleta</button>
       </div>
     </header>
+    {displayModalCreate && <ModalNewIceCream closeModal = {handleDisplayModalCreate}/>}
+  </>
   );
 };
 
---
+++ src/components/Modals/NewIceCream/index.jsx
@@ -0,0 +1,96 @@
+import "./style.css";
+import { useState } from "react";
+import { toast } from "react-hot-toast";
+
+const ModalNewIceCream = ({ closeModal }) => {
+    const [sabor, setSabor] = useState("");
+    const [preco, setPreco] = useState(0);
+    const [descricao, setDescricao] = useState("");
+    const [foto, setFoto] = useState("");
+
+    const handleCreateIceCream = async() =>{
+        const newIceCream = {
+            sabor,
+            preco,
+            descricao,
+            foto
+        };
+
+        const response = await fetch("http://localhost:3005/paletas/criar-paleta",
+        {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            mode: "cors",
+            body: JSON.stringify(newIceCream),
+        });
+
+        if( response.status !== 201) {
+          return toast.error("Erro ao adcionar nova paleta!");
+        };
+
+        setSabor("");
+        setPreco("");
+        setDescricao("");
+        setFoto("");
+        closeModal();
+        toast.success("Paleta adicionada com sucesso!!!");
+        
+    };
+
+  return (
+    <div className="box-modal">
+      <div className="modal-container">
+        <button onClick={closeModal}>Fechar</button>
+        <form>
+          <fieldset>
+            <p>Preencha o formulário</p>
+
+            <label>Sabor: </label>
+            <input 
+              value={sabor}
+            onChange={(event) => setSabor(event.target.value)} 
+            name = "sabor"
+            required/>
+
+            <label>Preço:</label>
+            <input 
+            value={preco}
+            onChange={(event) => setPreco(event.target.value)} 
+            type="number" 
+            name = "preco"
+            required/>
+
+            <label>Descrição: </label>
+            <input 
+            value={descricao}
+            onChange={(event) => setDescricao(event.target.value)} 
+            name = "descricao"
+            required />
+
+            <label>Foto: </label>
+            <input 
+            value={foto} 
+            onChange={(event) => setFoto(event.target.value)} 
+            name = "foto"
+            required/>
+
+            <button onClick={(handleCreateIceCream)}>Cadastrar</button>
+          </fieldset>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default ModalNewIceCream;
+
+// {
+//   /* <div>
+//                     <h3> Adcionar ao cardápio</h3>
+//                     <button> X </button>
+//                 </div>
+//                 <input></input>
+//                 <input></input> */
+// }
