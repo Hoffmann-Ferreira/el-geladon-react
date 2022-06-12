329180775c064f25ac8d73e9fa8a3e709959ebd5
Hoffmann-Ferreira <andreihf@outlook.com>
2022-06-12 17:48:57 -0300

    feat: Filtro dinâmico

--- src/components/Main/index.jsx
+++ src/components/Main/index.jsx
@@ -1,17 +1,32 @@
 import "./style.css";
 import Card from "../Card";
+import { useState } from "react";
 
-const Main = ({iceCream}) => {
+const Main = ({ iceCream }) => {
+  const [filterInput, setFilterInput] = useState("");
+
   return (
     <main>
       <div className="conteudo">
         <div className="Sub-titulo">
           <h2> Sabores de paletas</h2>
         </div>
+        <input
+          value={filterInput}
+          onChange={(event) => setFilterInput(event.target.value)}
+          placeholder="Digite o sabor da paleta"
+        />
         <div className="container-paletas">
-          {iceCream.map((element) => {
-            return <Card key={element._id} iceCream={element} />;
-          })}
+          {filterInput !== ""
+            ? iceCream
+                .filter((element) =>
+                 element.sabor.toLowerCase().includes(filterInput.toLowerCase()))
+                .map((element) => {
+                  return <Card key={element._id} iceCream={element} />;
+                })
+            : iceCream.map((element) => {
+                return <Card key={element._id} iceCream={element} />;
+              })}
         </div>
       </div>
     </main>
